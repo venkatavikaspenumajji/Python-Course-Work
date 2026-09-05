b2218f05b295a165eabb5cfea47bee22d65a1522
venkatavikaspenumajji <venkatavikaspenumajji@gmail.com>
2026-09-05 16:21:41 +0530

    listcom

---
+++ Day-23/listcom.py
@@ -0,0 +1,25 @@
+'''res = [i for i in range(1,11)]
+print(res)'''
+
+'''n=12
+res = [i for i in range(1,n+1) if n%i==0]
+print(res)'''
+
+'''r = [12,34,56,78,90,23,43,54]
+res = [i if i%2==0 else 0 for i in r]
+print(res)'''
+
+
+'''l = [int(input(f"enter the number - {i+1}:")) for i in range(10)]
+print(l)'''
+
+
+'''l = [input("enter the student name:") for i in range(10)]
+print(l)'''
+
+
+
+'''names = { input(f"enter the name - {i+1}: "):
+         int(input("enter the marks:"))
+         for i in range(5)}
+print(names)'''
