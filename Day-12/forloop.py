b3b2dcee134445bf02400dee3bf776d39ec0f66a
venkatavikaspenumajji <venkatavikaspenumajji@gmail.com>
2026-09-05 16:04:39 +0530

    for & nestedif

---
+++ Day-12/forloop.py
@@ -0,0 +1,50 @@
+l=[24,23,45,10,1,34,35,67]
+for i in l:
+        if i%2==0:
+           print(i,"even")
+else:
+           print(i,"odd")
+           '''
+'''
+marks = [45,56,78,90,23,34]
+for mark in marks:
+    if mark>35:
+        print(mark,"pass")
+    else:
+        print(mark,"fail")
+        '''
+'''
+bus ={'s1':'Avaliable','s2':'book','s3':'Avaliable','s4':'book'}
+for seat in bus:
+    if bus.get(seat)== 'Avaliable':
+        print(seat,bus.get(seat))
+'''
+
+
+'''
+n= int(input("enter a number:"))
+for i in range(1,21):
+    print(f"{n} x {i}= {n * i}")
+'''
+'''
+for i in range(1, 10 ):
+    print(" i")
+'''
+'''
+for i in range(10,1):
+    print("i")
+'''
+'''
+for i in range(1, 101,2):
+    print(i)
+
+
+for i in range(1,100,2):
+    print(i)
+
+
+
+
+
+
+
---
+++ Day-12/nestedif.py
@@ -0,0 +1,30 @@
+'''fa = eval(input("follows Account:"))
+cf = eval(input("close friend:"))
+if fa:
+    if cf:
+        print("story visible")
+    else:
+        print("not in close friend")
+else:
+    print("follow account")
+'''
+
+'''reg = eval (input("regiseter:"))
+fp = eval (input("fee paid:"))
+if reg:
+    if fp:
+        print("tournament entry ")
+    else:
+        print("entry fee")
+else:
+    print("register required")
+'''
+la = eval(input("link Active:"))
+pg = eval(input("permission:"))
+if la:
+    if pg:
+        print("file open ")
+    else:
+        print("Access")
+else:
+    print("invalid file") 
