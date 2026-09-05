311129d5fed11e96574ab4da1ee90fbc213666b3
venkatavikaspenumajji <venkatavikaspenumajji@gmail.com>
2026-09-05 16:19:05 +0530

    lambda

---
+++ Day-21/lambda.py
@@ -0,0 +1,47 @@
+'''greater = lambda a,b: a if a>b else b
+
+print(greater(12,13))
+print(greater(50,70))
+print(greater(40,20))
+print(greater(16,26))
+
+
+wish = lambda name: f'welcome to the course{name}'
+print(wish("vikas"))
+print(wish("Avinash"))
+print(wish("Anil"))
+print(wish("Lokesh"))
+
+iseven = lambda n:"even" if n%2==0 else "odd"
+
+print(iseven(45))
+print(iseven(48))
+print(iseven(10))
+print(iseven(56))
+
+avg =lambda a,b,c:(a+b+c)/3
+print(avg(4,5,6))
+print(avg(89,90,45))'''
+
+
+'''gst = lambda price : price + price*0.18
+
+print(gst(1000))
+print(gst(5000))
+print(gst(8000))'''
+
+
+
+
+'''prices = [3456,8765,5467,124,5678]
+
+res = list(map(lambda price : price + price*0.18,prices))
+print(res)'''
+
+
+
+
+
+
+
+
