1d62940867e7c5022560c5dbe1387787be72ac16
venkatavikaspenumajji <venkatavikaspenumajji@gmail.com>
2026-09-05 15:57:32 +0530

    strings

---
+++ Day-06/strings.py
@@ -0,0 +1,37 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> c = 'strings.py'
+... c.startswith('str')
+... True
+... c.startswith('Python')
+... False
+... s.endswith('.py')
+... Traceback (most recent call last):
+...   File "<pyshell#3>", line 1, in <module>
+...     s.endswith('.py')
+... NameError: name 's' is not defined
+... c.endswith('.py')
+... True
+... c.islower()
+... True
+... c.isupper()
+... False
+... 'PYTHONV13'.isupper()
+... True
+... c.isalpha()
+... False
+... c.isalnum()
+... False
+... c.isspace()
+... False
+... ' '.isspace()
+... True
+... c.istitle()
+... False
+... 'Python Is Easy To Learn'.istitle()
+... True
+... 'Python is east to learn'.istitle()
+... False
+... c.isidentifier()
+... False
+... 'variable1'.isidentifier()
